ede3ce21bc4a9eb523111146db720ab1d3dc4954
Jakub Neumann <4jakub.neumann7@gmail.com>
2023-04-05 02:47:10 +0200

    [CLEANUP] Cleaned up Header

--- src/containers/header/header_ui.tsx
+++ src/containers/header/header_ui.tsx
@@ -1,14 +1,9 @@
 import { MonthSelector } from "../../components/month_selector/month_selector";
 import { ViewSelector } from "../../components/view_selector/view_selector";
 import { Box } from "@mui/material";
-import { GridViews } from "../calendar/calendar_context";
 import { TodayButton } from "../../components/today_button/today_button";
 
-type HeaderUIProps = {
-  onViewButtonClick: (option: GridViews) => void;
-};
-
-export const HeaderUI = ({ onViewButtonClick }: HeaderUIProps) => {
+export const HeaderUI = () => {
   return (
     <Box
       component="header"
