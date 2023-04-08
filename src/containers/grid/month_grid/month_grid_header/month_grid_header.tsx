fcad9ead46e01cc9ac1b2b59d5cb17a86c6f374c
Jakub Neumann <4jakub.neumann7@gmail.com>
2023-04-08 18:07:57 +0200

    [MAINTENANCE] Updated MonthGridHeader visuals

--- src/containers/grid/month_grid/month_grid_header/month_grid_header.tsx
+++ src/containers/grid/month_grid/month_grid_header/month_grid_header.tsx
@@ -1,9 +1,10 @@
 import { MonthGridHeaderUI } from "./month_grid_header_ui";
 import { dayNames } from "../../../../utils/utils";
+import { Box } from "@mui/material";
 
 export const MonthGridHeader = () => {
   const dayNameElements = dayNames.map((dayName, index) => (
-    <p key={index}>{dayName}</p>
+    <Box key={index}>{dayName}</Box>
   ));
   return <MonthGridHeaderUI dayNames={dayNameElements} />;
 };
