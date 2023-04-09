8d7cb82cc51c75bc7d42bf044d063afe76702d3c
Jakub Neumann <4jakub.neumann7@gmail.com>
2023-04-09 02:21:11 +0200

    [FEATURE] WeekGrid now correctly displays multi-day events

--- src/containers/grid/week_grid/week_grid_cell/week_grid_cell_event/week_grid_cell_event_hook.tsx
+++ src/containers/grid/week_grid/week_grid_cell/week_grid_cell_event/week_grid_cell_event_hook.tsx
@@ -1,7 +1,12 @@
 export const useWeekGridCellEvent = (date: Date, from: Date, to: Date) => {
-  let fromHours = from.getHours() * 2;
-  from.getMinutes() >= 30 && fromHours++;
-  const fromGrid = fromHours;
+  let fromGrid;
+  if (date.getDate() > from.getDate()) {
+    fromGrid = 0;
+  } else {
+    let fromHours = from.getHours() * 2;
+    from.getMinutes() >= 30 && fromHours++;
+    fromGrid = fromHours;
+  }
 
   let toGrid;
   if (date.getDate() < to.getDate()) {
