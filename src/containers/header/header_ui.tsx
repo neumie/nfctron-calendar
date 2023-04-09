9a88113d71ee56e71899d41a3057affd3866cb13
Jakub Neumann <4jakub.neumann7@gmail.com>
2023-04-09 22:29:09 +0200

    [FEATURE] Responsive header

--- src/containers/header/header_ui.tsx
+++ src/containers/header/header_ui.tsx
@@ -5,17 +5,32 @@ import { TodayButton } from '../../components/today_button/today_button';
 
 const styles = {
   container: {
-    display: 'flex',
+    display: {
+      xs: 'grid',
+      md: 'flex',
+    },
+    gap: 2,
+    gridTemplateColumns: '1fr 1fr',
     alignItems: 'center',
     justifyContent: 'space-between',
     padding: '1em 2em',
+  },
+  centered: {
+    display: 'flex',
+    justifyContent: 'center',
   },
 };
 
 export const HeaderUI = () => (
   <Box component='header' sx={styles.container}>
-    <TodayButton />
-    <DateSelector />
-    <ViewSelector />
+    <Box sx={[styles.centered, { gridColumn: '1' }]}>
+      <TodayButton />
+    </Box>
+    <Box sx={[styles.centered, { gridRow: '2', gridColumn: '1 / -1' }]}>
+      <DateSelector />
+    </Box>
+    <Box sx={styles.centered}>
+      <ViewSelector />
+    </Box>
   </Box>
 );
