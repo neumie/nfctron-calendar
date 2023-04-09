8eeda1fd9f2a97b3ba1dda431f8b07e9652ad46b
Jakub Neumann <4jakub.neumann7@gmail.com>
2023-04-09 16:38:14 +0200

    [MAINTENANCE] EventEditor now has only one state

--- src/components/event_editor/event_editor_hook.tsx
+++ src/components/event_editor/event_editor_hook.tsx
@@ -9,28 +9,36 @@ import dayjs from 'dayjs';
 export const useEventEditor = () => {
   const { setCalendarState, activeDate, events, eventEditorLock, selectedEventId } =
     useCalendarContext();
-  const [eventTitle, setEventTitle] = useState('');
-  const [eventFromDate, setEventFromDate] = useState(activeDate);
-  const [eventFromTime, setEventFromTime] = useState(activeDate);
-  const [eventToDate, setEventToDate] = useState(activeDate);
-  const [eventToTime, setEventToTime] = useState(activeDate);
-  const [eventColor, setEventColor] = useState('');
+  const [eventState, setEventState] = useState({
+    title: '',
+    fromDate: activeDate,
+    fromTime: activeDate,
+    toDate: activeDate,
+    toTime: activeDate,
+    color: '#0693E3',
+  });
+  // const [eventTitle, setEventTitle] = useState('');
+  // const [eventFromDate, setEventFromDate] = useState(activeDate);
+  // const [eventFromTime, setEventFromTime] = useState(activeDate);
+  // const [eventToDate, setEventToDate] = useState(activeDate);
+  // const [eventToTime, setEventToTime] = useState(activeDate);
+  // const [eventColor, setEventColor] = useState('');
 
-  const eventFromDateDayjs = dayjs(eventFromDate);
-  const eventFromTimeDayjs = dayjs(eventFromTime);
-  const eventToDateDayjs = dayjs(eventToDate);
-  const eventToTimeDayjs = dayjs(eventToTime);
+  const eventFromDateDayjs = dayjs(eventState.fromDate);
+  const eventFromTimeDayjs = dayjs(eventState.fromTime);
+  const eventToDateDayjs = dayjs(eventState.toDate);
+  const eventToTimeDayjs = dayjs(eventState.toTime);
 
   const addEvent = () => {
     //Make the Event object
-    const eventFrom = combineDates(eventFromDate, eventFromTime);
-    const eventTo = combineDates(eventToDate, eventToTime);
+    const eventFrom = combineDates(eventState.fromDate, eventState.fromTime);
+    const eventTo = combineDates(eventState.toDate, eventState.toTime);
     const newEvent: Event = {
       id: crypto.randomUUID(),
-      title: eventTitle,
+      title: eventState.title,
       from: eventFrom,
       to: eventTo,
-      color: eventColor,
+      color: eventState.color,
     };
 
     setCalendarState({ events: [...events, newEvent] });
@@ -44,12 +52,14 @@ export const useEventEditor = () => {
 
   const resetEditor = () => {
     setCalendarState({ selectedEventId: '' });
-    setEventTitle('');
-    setEventFromDate(activeDate);
-    setEventFromTime(activeDate);
-    setEventToDate(activeDate);
-    setEventToTime(activeDate);
-    setEventColor('');
+    setEventState({
+      title: '',
+      fromDate: activeDate,
+      fromTime: activeDate,
+      toDate: activeDate,
+      toTime: activeDate,
+      color: '#0693E3',
+    });
   };
 
   const handleEventAdd = () => addEvent();
@@ -57,50 +67,54 @@ export const useEventEditor = () => {
   const handleExitEditMode = () => resetEditor();
 
   const handleEventTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEventTitle(event.target.value);
+    setEventState((prev) => ({ ...prev, title: event.target.value }));
   };
 
   const handleEventFromChange = (newEventFromDate: Dayjs | null) => {
     setCalendarState({ eventEditorLock: true });
-    newEventFromDate && setEventFromDate(newEventFromDate.toDate());
+    newEventFromDate && setEventState((prev) => ({ ...prev, fromDate: newEventFromDate.toDate() }));
   };
 
   const handleEventFromTimeChange = (newEventFromTime: Dayjs | null) => {
-    newEventFromTime && setEventFromTime(newEventFromTime.toDate());
+    newEventFromTime && setEventState((prev) => ({ ...prev, fromTime: newEventFromTime.toDate() }));
   };
 
   const handleEventToChange = (newEventToDate: Dayjs | null) => {
     setCalendarState({ eventEditorLock: true });
-    newEventToDate && setEventToDate(newEventToDate.toDate());
+    newEventToDate && setEventState((prev) => ({ ...prev, toDate: newEventToDate.toDate() }));
   };
 
   const handleEventToTimeChange = (newEventToTime: Dayjs | null) => {
-    newEventToTime && setEventToTime(newEventToTime.toDate());
+    newEventToTime && setEventState((prev) => ({ ...prev, toTime: newEventToTime.toDate() }));
   };
 
   const handleEventColorChange = (newEventColor: ColorResult) => {
-    setEventColor(newEventColor.hex);
+    setEventState((prev) => ({ ...prev, color: newEventColor.hex }));
   };
 
   useEffect(() => {
     if (eventEditorLock) return;
 
     const newEventDate = new Date(activeDate.getTime());
-    setEventFromDate(newEventDate);
-    setEventToDate(newEventDate);
+    setEventState((prev) => ({ ...prev, fromDate: newEventDate, toDate: newEventDate }));
   }, [activeDate]);
 
   useEffect(() => {
     const event = findEvent(events, selectedEventId);
     if (!event) return;
 
-    setEventTitle(event.title);
-    setEventFromDate(event.from);
-    setEventFromTime(event.from);
-    setEventToDate(event.to);
-    setEventToTime(event.to);
-    setEventColor(event.color);
+    setEventState({
+      title: event.title,
+      fromDate: event.from,
+      fromTime: event.from,
+      toDate: event.to,
+      toTime: event.to,
+      color: event.color,
+    });
   }, [selectedEventId]);
+
+  const eventTitle = eventState.title;
+  const eventColor = eventState.color;
 
   return {
     selectedEventId,
