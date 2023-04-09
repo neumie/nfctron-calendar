33c1a4c2ac881368e3a2c0c81ba48a94105c20e5
Jakub Neumann <4jakub.neumann7@gmail.com>
2023-04-09 21:18:32 +0200

    [MAINTENANCE] Removed React.Strictmode

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,10 @@ import { CssBaseline } from '@mui/material';
 import { Calendar } from './containers/calendar/calendar';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <>
     <CssBaseline />
     <Calendar />
-  </React.StrictMode>,
+  </>,
+  //</React.StrictMode>
 );
