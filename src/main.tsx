ea1dd2bc47ebe964b5338c34f24c970e47256ebb
Jakub Neumann <4jakub.neumann7@gmail.com>
2023-04-09 03:29:38 +0200

    [MAINTEANCE] Code cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import { CssBaseline } from '@mui/material';
 import { Calendar } from './containers/calendar/calendar';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  //<React.StrictMode>
-  <>
+  <React.StrictMode>
     <CssBaseline />
     <Calendar />
-  </>,
-  //</React.StrictMode>
+  </React.StrictMode>,
 );
